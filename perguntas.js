3dc949d7150ed507308aa7f5b9a207079b54224e
lulubebe <140447691+lulubebe@users.noreply.github.com>
2024-11-04 16:19:28 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 6:2?',
+    '3'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2?',
+    '4'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
